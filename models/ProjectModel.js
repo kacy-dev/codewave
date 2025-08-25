72a364428545c85c135979daf2fa4491b6c1b80f
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-08-25 13:33:17 +0100

    Update ProjectModel.js

--- models/ProjectModel.js
+++ models/ProjectModel.js
@@ -1,6 +1,54 @@
+// const mongoose = require('mongoose');
+
+// const ProjectModel = new mongoose.Schema(
+//     {
+//         title: {
+//             type: String,
+//             required: true,
+//             trim: true,
+//             unique: true,
+//             maxlength: 100,
+//         },
+//         description: {
+//             type: String,
+//             required: true,
+//             trim: true,
+//         },
+//         previewLink: {
+//             type: String,
+//             required: true,
+//             // match: [/^https?:\/\/[a-zA-Z0-9.-]+(?:\/[a-zA-Z0-9&%=~_.-]*)*/, 'Invalid URL format'],
+//         },
+//         imageUrl: {
+//             type: String,
+//             required: true,
+//             match: [/^https?:\/\/[a-zA-Z0-9.-]+(?:\/[a-zA-Z0-9&%=~_.-]*)*/, 'Invalid image URL format'],
+//         },
+//         createdAt: {
+//             type: Date,
+//             default: Date.now,
+//         },
+//         updatedAt: {
+//             type: Date,
+//             default: Date.now,
+//         },
+//     },
+//     { timestamps: true }
+// );
+
+// ProjectModel.pre('save', function (next) {
+//     this.updatedAt = Date.now();
+//     next();
+// });
+
+// const Projects = mongoose.model('Projects', ProjectModel);
+
+// module.exports = Projects;
+
+
 const mongoose = require('mongoose');
 
-const ProjectModel = new mongoose.Schema(
+const ProjectSchema = new mongoose.Schema(
     {
         title: {
             type: String,
@@ -17,30 +65,17 @@ const ProjectModel = new mongoose.Schema(
         previewLink: {
             type: String,
             required: true,
-            // match: [/^https?:\/\/[a-zA-Z0-9.-]+(?:\/[a-zA-Z0-9&%=~_.-]*)*/, 'Invalid URL format'],
         },
         imageUrl: {
             type: String,
             required: true,
             match: [/^https?:\/\/[a-zA-Z0-9.-]+(?:\/[a-zA-Z0-9&%=~_.-]*)*/, 'Invalid image URL format'],
         },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        updatedAt: {
-            type: Date,
-            default: Date.now,
-        },
     },
-    { timestamps: true }
+    { timestamps: true } 
 );
 
-ProjectModel.pre('save', function (next) {
-    this.updatedAt = Date.now();
-    next();
-});
-
-const Projects = mongoose.model('Projects', ProjectModel);
+const Projects = mongoose.model('Projects', ProjectSchema);
 
 module.exports = Projects;
+
